9289d6cbba65ac9a183988de9f598835b1712c2e
Administrator <uiui1999vn@gmail.com>
2020-10-28 23:23:33 +0700

    update config

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
-# HYPERPARAMETER for training
-NUM_EPOCHES = 10
+#  for training
+NUM_EPOCHS = 10
 LEARNING_RATE = 0.001
 MOMENTUM = 0.9
 SAVE_CHECKPOINT_PATH = './checkpoint/cifar_net.pth'
 OPTIMIZER = 'SGD'  # choose ['SGD', 'Adam']
+NUM_WORKERS = 0
+BATCH_SIZE = 4
+CUDA_DEVICE = '0'
 
 # For testing
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
