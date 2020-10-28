f76492411f701a7688201f269ddb8a267ab1c0f4
Administrator <uiui1999vn@gmail.com>
2020-10-28 23:23:51 +0700

    update eval

--- evaluate.py
+++ evaluate.py
@@ -13,12 +13,16 @@ if __name__ == '__main__':
     testset = torchvision.datasets.CIFAR10(root='./data', train=False,
                                            download=True, transform=transform)
 
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+    testloader = torch.utils.data.DataLoader(testset, batch_size=BATCH_SIZE,
                                              shuffle=False, num_workers=0)
+    # Instantiating CUDA device
+    device = torch.device("cuda:{}".format(CUDA_DEVICE) if torch.cuda.is_available() else "cpu")
     model = AlexNet()
+    model.to(device)
     model.classifier[4] = nn.Linear(4096, 1024)
     model.classifier[6] = nn.Linear(1024, 10)
-    model.load_state_dict(torch.load(PATH))
+    model.load_state_dict(torch.load(PATH, map_location=torch.device(device)))
+
     with torch.no_grad():
         for data in testloader:
             images, labels = data
