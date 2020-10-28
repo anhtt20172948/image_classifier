eeee43cb66a96e797aba1bdb05b0108487fedb8b
Administrator <uiui1999vn@gmail.com>
2020-10-28 22:17:11 +0700

    utils

---
+++ utils.py
@@ -0,0 +1,17 @@
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
