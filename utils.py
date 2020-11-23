8ca9e8cacae361a6c68b2bfe5a4b235badee2217
Administrator <uiui1999vn@gmail.com>
2020-11-23 14:47:50 +0700

    update image size

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import numpy as np
 
 transform = transforms.Compose([
     transforms.Resize(256),
-    transforms.CenterCrop(224),
+    transforms.CenterCrop(227),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
