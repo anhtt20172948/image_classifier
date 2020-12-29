ad7801801a5a20bef08973903938a51021eb0417
Administrator <uiui1999vn@gmail.com>
2020-12-29 09:14:03 +0700

    SAVE_CHECKPOINT_PATH from 'checkpoint name' to ' checkpoint folder'

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 NUM_EPOCHS = 10
 LEARNING_RATE = 0.001
 MOMENTUM = 0.9
-SAVE_CHECKPOINT_PATH = './checkpoint/cifar_net.pth'
+SAVE_CHECKPOINT_PATH = './checkpoint'
 OPTIMIZER = 'SGD'  # choose ['SGD', 'Adam']
 NUM_WORKERS = 0
 BATCH_SIZE = 4
@@ -10,5 +10,5 @@ CUDA_DEVICE = '0'
 
 # For testing
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-IMG_PATH = './images/horse.jpg'
+IMG_PATH = './images/bird.jpg'
 CHECKPOINT_PATH = './checkpoint/cifar_net.pth'
